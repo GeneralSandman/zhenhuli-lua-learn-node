04c301da49d0e0469ea72eaa96811ba6691ac20e
GeneralSandman <generalsandman@163.com>
2023-05-30 10:29:31 +0800

    update:

--- ch02/parser.cpp
+++ ch02/parser.cpp
@@ -4,7 +4,6 @@
 
 typedef unsigned char byte;
 typedef long int int64;
-typedef float float64;
 typedef unsigned int uint32;
 
 typedef struct BinaryChunk BinaryChunk;
@@ -44,7 +43,7 @@ struct Header {
     byte luaIntegerSize;
     byte luaNumberSize;
     int64 luacInt;
-    float64 luacNum;
+    double luacNum;
 };
 
 struct Prototype {
@@ -92,7 +91,7 @@ struct Constant {
         byte byte_value;
         bool bool_value;
         int64_t int_value;
-        float64 number_value;
+        double number_value;
     } u;
 };
 
@@ -101,34 +100,6 @@ void Undump(byte data, size_t size, Prototype* proto) {
 
 }
 
-
-byte readByte() {
-
-}
-
-std::vector<byte> readBytes() {
-
-}
-
-uint32_t readUint32() {
-
-}
-
-uint64_t readUint64() {
-
-}
-
-int64_t readLuaInteger() {
-
-}
-
-float64 readLuaNumber() {
-
-}
-
-std::string readString() {
-
-}
 
 void checkHeader() {
 
@@ -168,25 +139,191 @@ std::vector<std::string> readUpvalueNames() {
 
 }
 
+std::string byte_to_string(std::vector<byte>& bytes) {
+    std::string result;
+    for(auto byte_value: bytes) {
+        // std::cout << "byte_value:" << byte_value << std::endl;
+        result += char(byte_value);
+    }
+    return result;
+}
+
+class FileBuffer {
+public:
+    size_t buffer_size;
+    byte* buffer;
+    size_t read_offset;
+
+    FileBuffer(size_t buffer_size_) {
+        buffer_size = buffer_size_;
+        buffer = (byte*)malloc(sizeof(byte)*buffer_size);
+        read_offset = 0;
+    }
+
+    size_t fill(FILE* input) {
+        return fread(buffer, 1, buffer_size, input);
+    }
+
+    ~FileBuffer() {
+        free(buffer);
+    }
+
+    byte readByte() {
+        byte byte_value = (byte)(this->buffer[read_offset]);
+        read_offset++;
+        return byte_value;
+    }
+
+    std::vector<byte> readBytes(uint n) {
+        std::vector<byte> res(n,0);
+        for(int i=0; i<n; i++) {
+            res[i] = (byte)(this->buffer[read_offset]);
+            read_offset++;
+        }
+        return res;
+    }
+
+    uint32_t readUint32() {
+        // 小端
+        uint32_t res = 0;
+        res = (res<<8)|(uint32_t)(this->buffer[read_offset+3]);
+        res = (res<<8)|(uint32_t)(this->buffer[read_offset+2]);
+        res = (res<<8)|(uint32_t)(this->buffer[read_offset+1]);
+        res = (res<<8)|(uint32_t)(this->buffer[read_offset+0]);
+        read_offset += 4;
+        return res;
+    }
+
+    uint64_t readUint64() {
+        // 小端
+        uint64_t res = 0;
+        res = (res<<8)|(uint64_t)(this->buffer[read_offset+7]);
+        res = (res<<8)|(uint64_t)(this->buffer[read_offset+6]);
+        res = (res<<8)|(uint64_t)(this->buffer[read_offset+5]);
+        res = (res<<8)|(uint64_t)(this->buffer[read_offset+4]);
+        res = (res<<8)|(uint64_t)(this->buffer[read_offset+3]);
+        res = (res<<8)|(uint64_t)(this->buffer[read_offset+2]);
+        res = (res<<8)|(uint64_t)(this->buffer[read_offset+1]);
+        res = (res<<8)|(uint64_t)(this->buffer[read_offset+0]);
+        read_offset += 8;
+        return res;
+
+    }
+
+    int64_t readLuaInteger() {
+        return int64_t(readUint64());
+    }
+
+    double readLuaNumber() {
+        uint64_t value = readUint64();
+        // std::cout << "int value:" << value << std::endl;
+        // std::cout << "float value:" << *(double*)(&value) << std::endl;
+        return *(double*)(&value);
+    }
+
+    std::string readString() {
+        byte size = uint(readByte());
+        if(size == 0) {
+            return "";
+        } else if (size == 0XFF) {
+            size = uint(readUint64());
+        }
+
+        std::string res;
+        std::vector<byte> bytes = readBytes(size-1);
+        for(auto byte_value: bytes) {
+            res += std::to_string(byte_value);
+        }
+        return res;
+    }
+
+
+    int checkHeader() {
+        std::vector<byte> bytes = readBytes(4);
+        std::string signature = byte_to_string(bytes);
+        if (signature != LUA_SIGNATURE) {
+            std::cout << "signature:" << signature << std::endl;
+            std::cout << "signature invalid" << std::endl;
+            return -1;
+        }
+
+        if((int)readByte() != LUAC_VERSION) {
+            std::cout << "LUAC_VERSION invalid" << std::endl;
+            return -1;
+        }
+
+        if((int)readByte() != LUAC_FORMAT) {
+            std::cout << "LUAC_FORMAT invalid" << std::endl;
+            return -1;
+        }
+
+        bytes = readBytes(6);
+        std::string luac_data = byte_to_string(bytes);
+        if (luac_data != LUAC_DATA) {
+            std::cout << "luac_data:" << luac_data << std::endl;
+            std::cout << "LUAC_DATA invalid" << std::endl;
+            return -1;
+        }
+
+        if((int)readByte() != CINT_SIZE) {
+            std::cout << "CINT_SIZE invalid" << std::endl;
+            return -1;
+        }
+
+        if((int)readByte() != CSIZET_SIZE) {
+            std::cout << "CSIZET_SIZE invalid" << std::endl;
+            return -1;
+        }
+        
+        if((int)readByte() != INSTRUCTION_SIZE) {
+            std::cout << "INSTRUCTION_SIZE invalid" << std::endl;
+            return -1;
+        }
+
+        if((int)readByte() != LUA_INTEGER_SIZE) {
+            std::cout << "LUA_INTEGER_SIZE invalid" << std::endl;
+            return -1;
+        }
+
+        if((int)readByte() != LUA_NUMBER_SIZE) {
+            std::cout << "LUA_NUMBER_SIZE invalid" << std::endl;
+            return -1;
+        }
+
+        if(readLuaInteger() != LUAC_INT) {
+            std::cout << "LUAC_INT invalid" << std::endl;
+            return -1;
+        }
+
+        if(readLuaNumber() != LUAC_NUM) {
+            std::cout << "LUAC_NUM invalid" << std::endl;
+            return -1;
+        }
+
+
+        std::cout << "checkHeader success" << std::endl;
+        return 0;
+        
+    }
+
+    void undump() {
+        checkHeader();
+    }
+};
+
 int main() {
     // read content from binary file
     std::string inputFileName = "./helloworld.luac";
-
     FILE* input = NULL;
     input = fopen(inputFileName.c_str(), "rb");
     if(input == NULL) {
         exit(1);
     }
 
-    size_t buffer_size = 1024*1024;
-    void* buffer = malloc(sizeof(byte)*buffer_size);
+    FileBuffer buffer(1024*1024);
+    size_t read_size = buffer.fill(input);
+    buffer.undump();
 
-
-    size_t read_size = fread(buffer, buffer_size, 100, input);
-
-    std::cout << "read_size:" << read_size << std::endl;
-
-
-    free(buffer);
+    // std::cout << "read_size:" << read_size << std::endl;
 
 }
