4a34216ea1a238e1d0a57668234fa62314a7de6d
GeneralSandman <generalsandman@163.com>
2023-05-30 09:17:18 +0800

    update

--- ch02/make.sh
+++ ch02/make.sh
@@ -4,6 +4,9 @@ luac -v
 luac -o helloworld.luac helloworld.lua
 
 
-go build -o lua_binchunk_parser main.go
+go build -o lua_binchunk_parser_go main.go
 
-./lua_binchunk_parser ./helloworld.luac
+./lua_binchunk_parser_go ./helloworld.luac
+
+
+# g++ -std=c++11 -o lua_binchunk_parser_cpp parser.cpp && ./lua_binchunk_parser_cpp
