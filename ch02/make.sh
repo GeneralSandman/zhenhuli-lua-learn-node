4544e9f070df0cc48a17c00d812c6426699024f7
GeneralSandman <generalsandman@163.com>
2023-05-31 10:06:50 +0800

    update: 基本用CPP完成了lua字节码的解析

--- ch02/make.sh
+++ ch02/make.sh
@@ -6,7 +6,10 @@ luac -o helloworld.luac helloworld.lua
 
 go build -o lua_binchunk_parser_go main.go
 
+
+echo "=============lua_binchunk_parser_go=============\n"
 ./lua_binchunk_parser_go ./helloworld.luac
 
 
-# g++ -std=c++11 -o lua_binchunk_parser_cpp parser.cpp && ./lua_binchunk_parser_cpp
+echo "=============lua_binchunk_parser_cpp=============\n"
+g++ -std=c++11 -o lua_binchunk_parser_cpp parser.cpp && ./lua_binchunk_parser_cpp
--- ch02/parser.cpp
+++ ch02/parser.cpp
@@ -9,7 +9,6 @@ typedef unsigned int uint32;
 typedef struct BinaryChunk BinaryChunk;
 typedef struct Header Header;
 typedef struct Prototype Prototype;
-typedef struct Upvalue Upvalue;
 typedef struct LocVar LocVar;
 
 
@@ -32,6 +31,28 @@ const int TAG_INTEGER   = 0x13;
 const int TAG_SHORT_STR = 0x04;
 const int TAG_LONG_STR  = 0x14;
 
+typedef enum {
+    CONSTANT_TYPE_NIL,
+    CONSTANT_TYPE_BOOLEAN,
+    CONSTANT_TYPE_INTEGER,
+    CONSTANT_TYPE_NUMBER,
+    CONSTANT_TYPE_SHORT_STR,
+    CONSTANT_TYPE_LONG_STR,
+} Constant_Type;
+
+
+class Constant {
+public:
+    Constant_Type type;
+    union {
+        byte byte_value;
+        bool bool_value;
+        int64_t int_value;
+        double number_value;
+        std::string* string_value;
+    } u;
+};
+
 struct Header {
     byte signature[4];
     byte version;
@@ -46,6 +67,11 @@ struct Header {
     double luacNum;
 };
 
+class Upvalue {
+public:
+    byte Instack;
+    byte Idx;
+};
 struct Prototype {
     std::string Source;
     uint32 LineDefined;
@@ -55,8 +81,13 @@ struct Prototype {
     byte IsVararg;
     byte MaxStackSize;
 
-    uint32* Code;
-    Upvalue* Upvalues;
+    std::vector<uint32> Code;
+    std::vector<Constant> Constants;
+    std::vector<Upvalue> Upvalues;
+    std::vector<Prototype> Protots;
+    std::vector<uint32_t> LineInfo;
+    std::vector<LocVar> LocVars;
+    std::vector<std::string> UpvalueNames; 
 };
 
 struct BinaryChunk {
@@ -65,10 +96,7 @@ struct BinaryChunk {
     Prototype* mainFunc;
 };
 
-struct Upvalue {
-    byte Instack;
-    byte Idx;
-};
+
 
 struct LocVar {
     std::string VarName;
@@ -76,24 +104,8 @@ struct LocVar {
     uint32 EndPC;
 };
 
-typedef enum {
-    CONSTANT_TYPE_NIL,
-    CONSTANT_TYPE_BOOLEAN,
-    CONSTANT_TYPE_INTEGER,
-    CONSTANT_TYPE_NUMBER,
-    CONSTANT_TYPE_SHORT_STR,
-    CONSTANT_TYPE_LONG_STR,
-} Constant_Type;
 
-struct Constant {
-    Constant_Type type;
-    union {
-        byte byte_value;
-        bool bool_value;
-        int64_t int_value;
-        double number_value;
-    } u;
-};
+
 
 std::string byte_to_string(std::vector<byte>& bytes) {
     std::string result;
@@ -118,6 +130,106 @@ public:
 
     size_t fill(FILE* input) {
         return fread(buffer, 1, buffer_size, input);
+    }
+
+    void undump() {
+        int res = checkHeader();
+        if(res != 0) {
+            printf("checkHeader error:%d\n", res);
+        }
+        readByte();
+        Prototype proto = readProto("");
+        list(proto);
+    }
+
+    void list(Prototype proto) {
+        printHeader(proto);
+        printCode(proto);
+        printDetail(proto);
+        for(auto proto : proto.Protots) {
+            list(proto);
+        }
+    }
+
+    void printHeader(Prototype proto) {
+        std::string funcType = "main";
+        if(proto.LineDefined > 0) {
+            funcType = "function";
+        }
+
+        std::string varargFlag = "";
+        if(proto.IsVararg > 0) {
+            varargFlag = "+";
+        }
+
+        printf("\n%s <%s:%d,%d> (%ld instructions)\n",
+		    funcType.c_str(), proto.Source.c_str(), proto.LineDefined, proto.LastLineDefined, proto.Code.size());
+
+        printf("%d%s params, %d slots, %ld upvalues, ",
+		    proto.NumParams, varargFlag.c_str(), proto.MaxStackSize, proto.Upvalues.size());
+
+        printf("%ld locals, %ld constants, %ld functions\n",
+		    proto.LocVars.size(), proto.Constants.size(), proto.Protots.size());
+    }
+
+    void printCode(Prototype proto) {
+        for(size_t pc = 0; pc < proto.Code.size(); pc++) {
+            std::string line = "-";
+            if(proto.LineInfo.size()) {
+                line = std::to_string(proto.LineInfo[pc]);
+            }
+            printf("\t%ld\t[%s]\t0x%08X\n", pc+1, line.c_str(), proto.Code[pc]);
+        }
+    }
+
+    void printDetail(Prototype proto) {
+	    printf("constants (%ld):\n", proto.Constants.size());
+        for(size_t i = 0; i<proto.Constants.size(); i++) {
+            printf("\t%ld\t%s\n", i+1, constantToString(proto.Constants[i]).c_str());
+        }
+
+        printf("locals (%ld):\n", proto.LocVars.size());
+        for(size_t i=0; i<proto.LocVars.size(); i++) {
+            printf("\t%ld\t%s\t%ud\t%d\n",
+	    		i, proto.LocVars[i].VarName.c_str(), proto.LocVars[i].StartPC+1, proto.LocVars[i].EndPC+1);
+        }
+
+	    printf("upvalues (%ld):\n", proto.Upvalues.size());
+        for(size_t i=0; i<proto.Upvalues.size(); i++) {
+	    	printf("\t%ld\t%s\t%d\t%d\n",
+	    		i, upvalName(proto, i).c_str(), proto.Upvalues[i].Instack, proto.Upvalues[i].Idx);
+	    }
+
+    }
+
+    std::string constantToString(Constant constant) {
+        switch(constant.type) {
+            case CONSTANT_TYPE_NIL:
+            return "nil";
+            break;
+            case CONSTANT_TYPE_BOOLEAN: 
+            return std::to_string(constant.u.bool_value);
+            break;
+            case CONSTANT_TYPE_INTEGER: 
+            return std::to_string(constant.u.int_value);
+            break;
+            case CONSTANT_TYPE_NUMBER: 
+            return std::to_string(constant.u.number_value);
+            break;
+            case CONSTANT_TYPE_SHORT_STR: 
+            return std::string(*constant.u.string_value);
+            break;
+            case CONSTANT_TYPE_LONG_STR: 
+            return std::string(*constant.u.string_value);
+            break;
+        }
+    }
+
+    std::string upvalName(Prototype proto, int idx) {
+        if(idx >= proto.UpvalueNames.size()){
+            return "-";
+        }
+        return proto.UpvalueNames[idx];
     }
 
     ~FileBuffer() {
@@ -172,8 +284,6 @@ public:
 
     double readLuaNumber() {
         uint64_t value = readUint64();
-        // std::cout << "int value:" << value << std::endl;
-        // std::cout << "float value:" << *(double*)(&value) << std::endl;
         return *(double*)(&value);
     }
 
@@ -188,7 +298,7 @@ public:
         std::string res;
         std::vector<byte> bytes = readBytes(size-1);
         for(auto byte_value: bytes) {
-            res += std::to_string(byte_value);
+            res += char(byte_value);
         }
         return res;
     }
@@ -256,48 +366,140 @@ public:
             return -1;
         }
 
-
-        std::cout << "checkHeader success" << std::endl;
         return 0;
-        
     }
 
-    void readProto(Prototype* proto) {
-        
+    Prototype readProto(std::string parentSource) {
+        std::string source = readString();
+        if (source == "") {
+            source = parentSource;
+        }
+
+        Prototype *proto = new Prototype;
+
+        proto->Source = source;
+        proto->LineDefined = readUint32();
+        proto->LastLineDefined = readUint32();
+        proto->NumParams = readByte();
+        proto->IsVararg = readByte();
+        proto->MaxStackSize = readByte();
+        proto->Code = readCode();
+        proto->Constants = readConstants();
+        proto->Upvalues = readUpvalues();
+        proto->Protots = readProtos(source);
+        proto->LineInfo = readLineInfo();
+        proto->LocVars = readLocVars();
+        proto->UpvalueNames = readUpvalueNames();
+
+        return *proto;
     }
 
     std::vector<uint32> readCode() {
-
+        uint32_t size = readUint32();
+        std::vector<uint32> res(size, 0);
+        for(uint32_t i=0; i<size; i++) {
+            res[i] = readUint32();
+        }
+        return res;
     }
 
     std::vector<Constant> readConstants() {
-
+        uint32_t size = readUint32();
+        std::vector<Constant> res(size);
+        for(uint32_t i=0; i<size; i++) {
+            res[i] = readConstant();
+        }
+        return res;
     }
 
-    void readConstants(Constant* constant) {
+    Constant readConstant() {
+        Constant res;
+        Constant_Type type = (Constant_Type)(readByte());
 
+        std::string* tmp = nullptr;
+
+        switch(type) {
+            case CONSTANT_TYPE_NIL: 
+            res.type = type;
+            break;
+            case CONSTANT_TYPE_BOOLEAN: 
+            res.type = type;
+            res.u.bool_value = (readByte() != 0);
+            break;
+            case CONSTANT_TYPE_INTEGER: 
+            res.type = type;
+            res.u.int_value = readLuaInteger();
+            break;
+            case CONSTANT_TYPE_NUMBER: 
+            res.type = type;
+            res.u.int_value = readLuaNumber();
+            break;
+            case CONSTANT_TYPE_SHORT_STR: 
+            res.type = type;
+            // std::cout << "readString:" << readString() << std::endl;
+            tmp = new std::string(readString());
+            res.u.string_value = tmp;
+            break;
+            case CONSTANT_TYPE_LONG_STR: 
+            res.type = type;
+            tmp = new std::string(readString());
+            res.u.string_value = tmp;
+            break;
+        }
+
+        return res;
     }
 
+    std::vector<Upvalue> readUpvalues() {
+        uint32_t size = readUint32();
+        std::vector<Upvalue> res(size);
+        for(uint32_t i=0; i<size; i++) {
+            res[i].Instack = readByte();
+            res[i].Idx = readByte();
+        }
+        return res;
+    }
 
-    std::vector<Prototype> readProtos() {
-
+    std::vector<Prototype> readProtos(std::string parentSource) {
+        uint32_t size = readUint32();
+        std::vector<Prototype> res(size);
+        for(uint32_t i=0; i<size; i++) {
+            res[i] = readProto(parentSource);
+        }
+        return res;
     }
 
     std::vector<uint32> readLineInfo() {
-
+        uint32_t size = readUint32();
+        std::vector<uint32> res(size, 0);
+        for(uint32_t i=0; i<size; i++) {
+            res[i] = readUint32();
+        }
+        return res;
     }
 
     std::vector<LocVar> readLocVars() {
-
+        uint32_t size = readUint32();
+        std::vector<LocVar> res(size);
+        for(uint32_t i=0; i<size; i++) {
+            res[i].VarName = readString();
+            res[i].StartPC = readUint32();
+            res[i].EndPC = readUint32();
+        }
+        return res;
     }
 
     std::vector<std::string> readUpvalueNames() {
+        uint32_t size = readUint32();
+        std::vector<std::string> res(size);
+        for(uint32_t i=0; i<size; i++) {
+            res[i] = readString();
+        }
+        return res;
 
     }
 
-    void undump() {
-        checkHeader();
-    }
+
 };
 
 int main() {
